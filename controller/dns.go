6c09f02179a6d6ac6d8f411f13032e96c2f0025e
ChimanJain <36687396+ChimanJain@users.noreply.github.com>
2020-08-10 22:03:05 +0530

    Changed the sleep time to sync waitgroup.

--- controller/dns.go
+++ controller/dns.go
@@ -5,7 +5,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"time"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +15,9 @@ var mxEntity model.MxEntity
 var dns model.Dns
 
 //FindIP Fetches the IP records
-func FindIP(url string) {
+func FindIP(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	iprecords, _ := net.LookupIP(url)
 	for _, ip := range iprecords {
 		dns.IPAddress = append(dns.IPAddress, ip)
@@ -24,12 +26,15 @@ func FindIP(url string) {
 
 //FindCName Fetches the CName records
 func FindCName(url string, cCName chan string) {
+
 	cname, _ := net.LookupCNAME(url)
 	cCName <- cname
 }
 
 //FindNS Fetches the NS records
-func FindNS(url string) {
+func FindNS(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	nameserver, _ := net.LookupNS(url)
 	for _, ns := range nameserver {
 		dns.NS = append(dns.NS, *&ns.Host)
@@ -37,7 +42,9 @@ func FindNS(url string) {
 }
 
 //FindMX Fetches the MX records
-func FindMX(url string) {
+func FindMX(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	mxrecords, _ := net.LookupMX(url)
 	for _, mx := range mxrecords {
 		mxEntity.Host, mxEntity.Pref = mx.Host, mx.Pref
@@ -46,32 +53,37 @@ func FindMX(url string) {
 }
 
 //FindTXT Fetches the TXT records
-func FindTXT(url string) {
+func FindTXT(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	txtrecords, _ := net.LookupTXT(url)
 	for _, txt := range txtrecords {
 		dns.TXT = append(dns.TXT, txt)
-		//fmt.Printf("%T\n", txt)
 	}
 }
 
 //FindDNS function is used for fetching the IP, CName, NS, MX and TXT records of the URL
 func FindDNS(c *gin.Context) {
 
+	var wg sync.WaitGroup
+
+	wg.Add(4)
 	encodedURL := url.QueryEscape(c.Param("url"))
 	dns.URL = c.Param("url")
 
-	go FindIP(encodedURL)
+	go FindIP(encodedURL, &wg)
 
 	cCName := make(chan string)
 	go FindCName(encodedURL, cCName)
 	dns.Cname = <-cCName
 
-	go FindNS(encodedURL)
+	go FindNS(encodedURL, &wg)
 
-	go FindMX(encodedURL)
+	go FindMX(encodedURL, &wg)
 
-	go FindTXT(encodedURL)
+	go FindTXT(encodedURL, &wg)
 
-	time.Sleep(1 * time.Second)
+	wg.Wait()
+
 	c.JSON(http.StatusOK, gin.H{"data": dns})
 }
